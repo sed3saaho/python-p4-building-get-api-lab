f86797ada25744939b7bc3806f19db1922322610
Sedly <sedly.keith@student.moringaschool.com>
2024-07-01 16:40:08 +0300

    pyhton-p4-building-get-api-lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,27 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return jsonify(bakeries)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    
+    bakery = Bakery.query.get(id)
+    baked_goods = [baked_good.to_dict() for baked_good in bakery.baked_goods]
+    return jsonify({
+        'baked_goods': baked_goods
+    })
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    return jsonify(baked_goods)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(baked_good.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
